7c4135b489ecdc8393cf2b0b60090f97826e5f5a
kwhou <kwhou@users.noreply.github.com>
2018-09-29 23:28:51 +0800

    quantize weight value to 8 bits integer

--- mnist.py
+++ mnist.py
@@ -9,43 +9,34 @@ x = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 W = tf.Variable(tf.random_normal([784,10], dtype=tf.float32))
-b = tf.Variable(tf.random_normal([10], dtype=tf.float32))
+# b = tf.Variable(tf.random_normal([10], dtype=tf.float32))
 
-y = tf.matmul(x,W)+b
+W_q = tf.fake_quant_with_min_max_args(W, min=-128.0, max=127.0, num_bits=8)
+# b_q = tf.fake_quant_with_min_max_args(b, min=-128.0, max=127.0, num_bits=8)
+
+# y = tf.matmul(x, W_q)#+b_q
+y = tf.matmul(tf.round(x*255)/255, W_q)#+b_q
 
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = y, labels = y_)
 loss = tf.reduce_sum(cross_entropy)
 
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-
-w_boundary = 5
-max_integer = 255
-scale_up = max_integer / (2 * w_boundary)
-
-clip_op = tf.assign(W, (tf.round((tf.clip_by_value(W, -w_boundary, w_boundary) + w_boundary) * scale_up) / scale_up) - w_boundary)
+train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 
 tf.global_variables_initializer().run()
 
-sess.run(clip_op)
-
 for i in range(5000):
   batch_xs, batch_ys = mnist.train.next_batch(500)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-  sess.run(clip_op)
 
 # print(sess.run(W).tolist())
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))*100
 
-x_data = mnist.test.images
-
-accuracy_new = sess.run(accuracy, feed_dict={x: x_data, y_: mnist.test.labels})
-print(accuracy_new)
+num_images = 100
 
 h = open("pixels.txt",'w');
-num_images = 100
 h.write(str(num_images)+"\n")
 for r in range(num_images):
   for k in range(784):
@@ -53,6 +44,22 @@ for r in range(num_images):
   h.write(str(mnist_one_hot_false.test.labels[r])+"\n")
 h.close()
 
+x_data = mnist.test.images[0:num_images, :]
+y_data = mnist.test.labels[0:num_images, :]
+
+accuracy_new = sess.run(accuracy, feed_dict={x: x_data, y_: y_data})
+print(accuracy_new)
+
+weights = sess.run(W_q)
+
+w_max = 0
+w_min = 0
+for i in range(0,784):
+  for j in range(0,10):
+    if weights[i][j] > w_max:
+      w_max = weights[i][j]
+    if weights[i][j] < w_min:
+      w_min = weights[i][j]
 
 accuracy_prev = 0
 g = open("accuracy.txt",'r')
@@ -66,12 +73,11 @@ if (accuracy_new>accuracy_prev):
   
   print ("Writing new weights to file")
   
-  weights = sess.run(W)
-  
   f = open("weights.txt",'w')
   for i in range(0,784):
     for j in range(0,10):
-      f.write(str( int((weights[i][j] + w_boundary) * scale_up))+" ")
+      f.write(str( int(weights[i][j] - w_min))+" ")
+      # f.write(str( weights[i][j] - w_min)+" ")
     f.write("\n")
   f.close()
   
